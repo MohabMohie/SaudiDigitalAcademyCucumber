0ea7656743b48d97dd089ee1bc655e94d0381902
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2024-03-26 11:10:55 +0200

    add filters using tag

--- src/test/java/seleniumCucumber/RunSeleniumTests.java
+++ src/test/java/seleniumCucumber/RunSeleniumTests.java
@@ -5,11 +5,14 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
+import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectPackages("seleniumCucumber")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber.html")
+@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME , value = "@Regression")
+
 public class RunSeleniumTests {
 }
